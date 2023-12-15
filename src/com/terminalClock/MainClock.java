c335e6107c1eded1844beb07f1fb87fa85cccf30
kosail <kamururi@protonmail.com>
2023-12-14 19:16:48 -0600

    added clear screen per second

--- src/com/terminalClock/MainClock.java
+++ src/com/terminalClock/MainClock.java
@@ -1,11 +1,18 @@
 package com.terminalClock;
 
 public class MainClock {
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static void main(String[] args) throws InterruptedException {
         Clock mainClock = new Clock();
         // mainClock.set12HoursClock();
 
         while (true) {
+            clearScreen();
             System.out.printf(mainClock.getTimeNow());
             Thread.sleep(1000);
         }
