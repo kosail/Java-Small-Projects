751946d983e6350ab3ab05fe7cd76241e214531c
kosail <kamururi@protonmail.com>
2024-01-14 17:45:09 -0600

    added arguments capability

--- terminalClock/src/com/terminalClock/MainClock.java
+++ terminalClock/src/com/terminalClock/MainClock.java
@@ -1,23 +1,55 @@
 package com.terminalClock;
 
+import com.consoleCustomPrint.ConsoleCustomPrint;
+import com.consoleCustomPrint.ForegroundColors;
+
 public class MainClock {
 
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
     public static void main(String[] args) throws InterruptedException {
-        Clock mainClock = new Clock();
-        UNIXCLIPrettifier unixP = new UNIXCLIPrettifier();
         
-        mainClock.set12HoursClock();
-        unixP.setMetalColors();
+        if (args.length == 0 || args.length > 3) {
+            usageManual.invalidArguments();
+        }
+        
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("--help")) {
+                usageManual.getUsage();
+            } else {
+                usageManual.invalidArguments();
+            }
+        }
+        
+        Clock mainClock = new Clock();
+        
+        if (args.length >= 2) {
+            if (args[1].equals("-12")) {
+                mainClock.set12HoursClock();
+            }
+        }
+        
+        if (args[0].equalsIgnoreCase("-windows")) {
+            while (true) {
+                ConsoleCustomPrint.clearScreen();
+                System.out.println(mainClock.getTimeNow());
+                Thread.sleep(990);
+            }
+            
+        } else if (args[0].equalsIgnoreCase("-unix")) {
+            UNIXCLIPrettifier unixP = new UNIXCLIPrettifier();
 
-        while (true) {
-            clearScreen();
-            unixP.UNIXPrettyPrinter(mainClock.getTimeNow());
-            Thread.sleep(990);
+            if (args.length == 3) {
+                if (args[2].equalsIgnoreCase("-metal")) {
+                    unixP.setMetalColors();
+                } else if (args[2].equalsIgnoreCase("-rainbow")) {
+                    unixP.setGayColors();
+                }
+            }
+            
+            while (true) {
+                ConsoleCustomPrint.clearScreen();
+                unixP.UNIXPrettyPrinter(mainClock.getTimeNow());
+                Thread.sleep(990);
+            }
         }
     }
 }
